8e63eae16b7e41a78ea19c36714c0e5b1d17ada1
Muhammed Kamal <muhammedkmal3@gmail.com>
2017-11-17 23:17:48 +0200

    refactor naming

--- PathFindingAlgorithem/Program.cs
+++ PathFindingAlgorithem/Program.cs
@@ -1,45 +1,46 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace PathFindingAlgorithem
 {
-	class Program
-	{
+    class Program
+    {
         static void Main(string[] args)
         {
-            Maze m = Maze.getMaze();
-            Vector v = new Vector(new Point(4, 0), Direction.North);
-            Console.WriteLine(m.getPath(v, new Point(0, 4)));
+            /*Maze m = Maze.getMaze();
+            Vector v = new Vector(new Point(0, 21), Direction.North);
+            Console.WriteLine(m.getPath(v, new Point(0, 0)));*/
         }
-	}
+    }
 
-	enum Direction
-	{
-		North,
-		South,
-		East,
-		West
-	}
-
-	class Point : ICloneable , IEquatable<Point>
+    enum Direction
     {
-		public Point(int x, int y)
-		{
-			this.x = x;
-			this.y = y;
-		}
+        North,
+        South,
+        East,
+        West
+    }
 
-		public Point() : this(0, 0) { }
-		public int x { get; set; }
-		public int y { get; set; }
+    class Point : ICloneable, IEquatable<Point>
+    {
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
 
-		public object Clone()
-		{
-			return new Point(this.x, this.y);
-		}
+        public Point() : this(0, 0) { }
+        public int x { get; set; }
+        public int y { get; set; }
 
-        public static bool compare(List<Point> list , Point p)
+        public object Clone()
+        {
+            return new Point(this.x, this.y);
+        }
+
+        public static bool hasPoint(List<Point> list, Point p)
         {
             return list.Find(item => item.Equals(p)) != null;
         }
@@ -50,100 +51,101 @@ namespace PathFindingAlgorithem
         }
     }
 
-	class Vector : ICloneable , IEqualityComparer<Vector>
+    class Vector : ICloneable, IEqualityComparer<Vector>
     {
         public Direction direction;
-		public Point position;
-        //public List<Vector> previous;
+        public Point position;
         public HashSet<Vector> previous;
         public Vector() { }
 
-		public Vector(Point position, Direction direction)
-		{
-			this.position = position;
-			this.direction = direction;
-            previous = new HashSet<Vector>(new Vector());//new List<Vector>();
+        public Vector(Point position, Direction direction)
+        {
+            this.position = position;
+            this.direction = direction;
+            previous = new HashSet<Vector>(new Vector());
         }
         public object Clone()
-		{
-			Point position = (Point)this.position.Clone();
-			return new Vector(position, this.direction);
-		}
+        {
+            Point position = (Point)this.position.Clone();
+            return new Vector(position, this.direction);
+        }
 
-		public Vector moveForword(Maze maze)
-		{
-			Vector newCar = (Vector)Clone();
+        public Vector moveForword(Maze maze)
+        {
+            Vector newCar = (Vector)Clone();
 
-			switch (this.direction)
-			{
-				case Direction.North:
-					newCar.position.x--;
-					break;
-				case Direction.East:
-					newCar.position.y++;
-					break;
-				case Direction.South:
-					newCar.position.x++;
-					break;
-				case Direction.West:
-					newCar.position.y--;
-					break;
-			}
-            if (newCar.canMove(maze) == true) {
-                return newCar;
+            switch (this.direction)
+            {
+                case Direction.North:
+                    newCar.position.x--;
+                    break;
+                case Direction.East:
+                    newCar.position.y++;
+                    break;
+                case Direction.South:
+                    newCar.position.x++;
+                    break;
+                case Direction.West:
+                    newCar.position.y--;
+                    break;
             }
-            else
-                return null;
-		}
-
-		public Vector moveRight(Maze maze)
-		{
-			Vector newCar = (Vector)Clone();
-
-			switch (this.direction)
-			{
-				case Direction.North:
-					newCar.direction = Direction.East;
-					newCar.position.y++;
-					break;
-				case Direction.East:
-					newCar.direction = Direction.South;
-					newCar.position.x++;
-					break;
-				case Direction.South:
-					newCar.direction = Direction.West;
-					newCar.position.y--;
-					break;
-				case Direction.West:
-					newCar.direction = Direction.North;
-					newCar.position.x--;
-					break;
-			}
-            if (newCar.canMove(maze) == true) { 
+            if (newCar.canMove(maze) == true)
+            {
                 return newCar;
             }
             else
                 return null;
         }
 
-		public bool isExist(Maze maze)
-		{
-			return maze.isExist(this.position);
-		}
+        public Vector moveRight(Maze maze)
+        {
+            Vector newCar = (Vector)Clone();
 
-		public bool isBlocked(Maze maze)
-		{
-			return maze.isBlocked(this.position);
-		}
+            switch (this.direction)
+            {
+                case Direction.North:
+                    newCar.direction = Direction.East;
+                    newCar.position.y++;
+                    break;
+                case Direction.East:
+                    newCar.direction = Direction.South;
+                    newCar.position.x++;
+                    break;
+                case Direction.South:
+                    newCar.direction = Direction.West;
+                    newCar.position.y--;
+                    break;
+                case Direction.West:
+                    newCar.direction = Direction.North;
+                    newCar.position.x--;
+                    break;
+            }
+            if (newCar.canMove(maze) == true)
+            {
+                return newCar;
+            }
+            else
+                return null;
+        }
 
-		public bool canMove(Maze maze)
-		{
-			return isExist(maze) && !isBlocked(maze);
-		}
+        public bool isExist(Maze maze)
+        {
+            return maze.isExist(this.position);
+        }
+
+        public bool isBlocked(Maze maze)
+        {
+            return maze.isBlocked(this.position);
+        }
+
+        public bool canMove(Maze maze)
+        {
+            return isExist(maze) && !isBlocked(maze);
+        }
 
         public bool isExistInList(List<Vector> list)
         {
-            return list.Find(c=> c.Equals(this) )!= null;
+            return list.Find(c => c.Equals(this)) != null;
         }
 
         public bool Equals(Vector other)
@@ -162,99 +164,106 @@ namespace PathFindingAlgorithem
         }
     }
 
-	class Maze
-	{
-		int[,] maze;
-		int width;
-		int height;
+    class Maze
+    {
+        int[,] maze;
+        int width;
+        int height;
 
-		public Maze(int width, int height)
-		{
-			this.width = width;
-			this.height = height;
-			this.maze = new int[width, height];
-		}
+        public Maze(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            this.maze = new int[width, height];
+        }
 
-		public static Maze getMaze()
-		{
-			Maze maze = new Maze(5, 5);
-			maze.maze = new int[,]{
-				{0, 0, 0, 0, 0},
-				{-1, -1, 0, -1, -1},
-				{0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0}
-			};
-			return maze;
-		}
+        public static Maze getMaze()
+        {
+            Maze maze = new Maze(4, 23);
+            maze.maze = new int[,]{
+                {0, 0, 0, 0, 0 ,0, 0, 0,0, 0, 0,0, 0, 0,0, 0, 0,0, 0, 0,0, 0, 0},
+                {0, -1, 0, 0, 0 ,0, 0, 0,0, 0, 0,0, 0, 0,0, 0, 0,0, 0, 0,0, 0, 0},
+                {0, 0, 0, 0, 0 ,0, 0, 0,0, 0, 0,0, 0, 0,0, 0, 0,0, 0, 0,0, 0, 0},
+                {0, 0, 0, 0, 0 ,0, 0, 0,0, 0, 0,0, 0, 0,0, 0, 0,0, 0, 0,0, 0, 0}
+            };
+            return maze;
+        }
 
-		public bool getPath(Vector start, Point finish)
-		{
+        public bool getPath(Vector start, Point finish)
+        {
             List<Vector> open = new List<Vector>();
             List<Vector> closed = new List<Vector>();
             Vector visited;
             open.AddRange(this.getNextMoves(start));
-            while(open.Count !=0)
+            while (open.Count != 0)
             {
                 visited = open[0];
                 open.RemoveAt(0);
                 if (finish.Equals(visited.position))
                 {
                     foreach (Vector vs in visited.previous)
-                        Console.WriteLine(vs.position.x + " , " + vs.position.y  + " : " + vs.direction.ToString());
+                        Console.WriteLine(vs.position.x + " , " + vs.position.y + " : " + vs.direction.ToString());
+                    Console.WriteLine(visited.position.x + " , " + visited.position.y + " : " + visited.direction.ToString());
                     return true;
                 }
-                foreach (Vector v in getNextMoves(visited)) { 
-                if (!v.isExistInList(closed) && !v.isExistInList(open)) { 
+                foreach (Vector v in getNextMoves(visited))
+                {
+                    if (!v.isExistInList(closed) && !v.isExistInList(open))
+                    {
                         open.Add(v);
                     }
                 }
                 closed.Add(visited);
             }
-			return false;
-		}
+            return false;
+        }
 
-		public List<Vector> getNextMoves(Vector car)
-		{
+        public List<Vector> getNextMoves(Vector car)
+        {
             List<Vector> moves = new List<Vector>();
             Vector forword = car.moveForword(this);
             Vector right = car.moveRight(this);
-			if (forword != null)
-			{
+            if (forword != null)
+            {
                 foreach (Vector pre in car.previous)
                     forword.previous.Add(pre);
                 forword.previous.Add(car);
                 moves.Add(forword);
-			}
-			if (right !=null)
-			{
+            }
+            if (right != null)
+            {
                 foreach (Vector pre in car.previous)
                     right.previous.Add(pre);
                 right.previous.Add(car);
                 moves.Add(right);
             }
-			return moves;
-		}
+            return moves;
+        }
 
-		public bool isBlocked(int x, int y)
-		{
-			return maze[x, y] == -1;
-		}
+        public bool isBlocked(int x, int y)
+        {
+            return maze[x, y] == -1;
+        }
 
-		public bool isBlocked(Point point)
-		{
-			return isBlocked(point.x, point.y);
-		}
+        public bool isBlocked(Point point)
+        {
+            return isBlocked(point.x, point.y);
+        }
 
-		public bool isExist(int x, int y)
-		{
-			return x >= 0 && x < width && y >= 0 && y < height;
-		}
+        public bool isExist(int x, int y)
+        {
+            return x >= 0 && x < width && y >= 0 && y < height;
+        }
 
-		public bool isExist(Point point)
-		{
-			return isExist(point.x, point.y);
-		}
+        public bool isExist(Point point)
+        {
+            return isExist(point.x, point.y);
+        }
 
-	}
+        public bool getPathParallel(Vector start, Point finish)
+        {
+
+            return false;
+        }
+    }
 }
