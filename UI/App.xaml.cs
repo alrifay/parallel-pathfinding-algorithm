ba4ee2c851e43f992e84158c2868a06aa1b26a44
Muhammed Kamal <muhammedkmal3@gmail.com>
2017-12-20 02:11:51 +0200

    One instance of app
    One instance open with Mutex

--- UI/App.xaml.cs
+++ UI/App.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
 using System.Windows;
+using System.Runtime.InteropServices;
 
 namespace UI
 {
@@ -13,5 +15,16 @@ namespace UI
     /// </summary>
     public partial class App : Application
     {
+        protected Mutex mutex;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            mutex = new Mutex(false, "Team@M Maze");
+            if (!mutex.WaitOne(0, false))
+            {
+                MessageBox.Show("App already running");
+                Environment.Exit(0);
+            }
+        }
     }
 }
